08d9b41c62f0c115063aa5219b4c20632c7b72fc
Adosio14 <dosioalvaromartin@gmail.com>
2024-09-23 17:39:17 -0300

    add new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
+    colors: {
+      "light-lavender": "#F5EFFF",
+      lavender: "#A594F9",
+      "lavender-dark": "#7A6ACD",
+      "indigo-light": "#5F67EC",
+      "lavender-pale": "#E5D9F2",
+      "indigo-bold": "#7358FC",
+    },
     extend: {},
   },
   future: {
